b4edf38ec0c18e2f6a0dc069273c5deeb21cd713
haggagdev <haggagdev@gmail.com>
2023-11-04 18:14:16 +0200

    convert to public

--- compute/workload.tf
+++ compute/workload.tf
@@ -1,28 +1,25 @@
 ## create GKE cluster with 3 nodes separated by zones in the same region
 # and stateful workload mongodb installed on each node with replica set configuration, 1 master and 2 slaves 
 
-
-
-
 resource "google_container_cluster" "gke_cluster" {
   name               = "workload-cluster"
   location           = var.workload-region
-  ## private cluster
 
   ## deletion protection disabled
   deletion_protection = false
   
 
-private_cluster_config {
-    enable_private_endpoint = true
-    enable_private_nodes   = true 
-    master_ipv4_cidr_block = "192.168.9.0/28"
+# private_cluster_config {
+#     enable_private_endpoint = true
+#     enable_private_nodes   = true 
+#     master_ipv4_cidr_block = "192.168.9.0/28"
 
 
-     master_global_access_config {
-    enabled = true
-  }
-  }
+#      master_global_access_config {
+#     enabled = true
+#   }
+#   }
+
   ip_allocation_policy {
     cluster_ipv4_cidr_block  = "192.167.0.0/21"
     services_ipv4_cidr_block = "192.166.0.0/21"
@@ -32,11 +29,13 @@ private_cluster_config {
   master_authorized_networks_config {
     cidr_blocks {
       cidr_block   = "10.1.0.0/28"
-      display_name = "net1"
+      display_name = "private_network"
     }
 
- 
-
+    cidr_blocks {
+      cidr_block   = "185.69.166.176/32"
+      display_name = "jenkins_server"
+    }
   }
 
   
--- network/firewall.tf
+++ network/firewall.tf
@@ -26,6 +26,7 @@ resource "google_compute_firewall" "workload-firewall-Ingress" {
     google_compute_subnetwork.managment_subnet.ip_cidr_range,
     "199.36.153.4/30", # ip ranges of artifact registry
     "199.36.153.8/30",
+    "185.69.166.176/32"
   ]
   # destination_ranges = [google_compute_subnetwork.workload_subnet.ip_cidr_range]
   target_service_accounts = [var.workload_service_account]
